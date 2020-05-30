dd7f46aeddeb9e761a07c840e98e6059072685d3
Ricardo Delfin <me@rdelfin.com>
2020-05-31 00:55:29 +0100

    Added logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #[macro_use]
 extern crate serde_derive;
+#[macro_use]
+extern crate log;
 
 mod config;
 mod ircdb;
@@ -7,6 +9,7 @@ mod tweeter;
 
 use crate::config::Settings;
 use ircdb::IrcDb;
+use log4rs;
 use std::time::Duration;
 use tokio::time::delay_for;
 use tweeter::Tweeter;
@@ -14,15 +17,25 @@ use tweeter::Tweeter;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let settings = Settings::new()?;
+    log4rs::init_file("config/log4rs.yml", Default::default())?;
     let tweeter = Tweeter::new(&settings);
     let db = IrcDb::new(&settings.db.file)?;
     let wait_time = Duration::from_secs(settings.interval_min * 60);
 
+    info!("Config finished...");
+
     loop {
         let quote = db.get_random(1, 270)?;
         let message = format!("#{}:\n{}", quote.id, quote.quote);
+        info!("Tweeting quote #{}: {}", quote.id, quote.quote);
         tweeter.send_tweet(message).await?;
+        info!("Sent tweet");
 
+        info!(
+            "Waiting for {}m {}s before sending next tweet",
+            wait_time.as_secs() / 60,
+            wait_time.as_secs() % 60
+        );
         delay_for(wait_time).await;
     }
 }
