9a8fc507b34022629c3f13092263328341ce2920
Ricardo Delfin <me@rdelfin.com>
2020-05-31 00:16:03 +0100

    Added basic main implementation of the tweeter

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,25 @@ mod config;
 mod ircdb;
 mod tweeter;
 
-fn main() {
-    println!("Hello, world!");
+use crate::config::Settings;
+use anyhow::Result;
+use ircdb::IrcDb;
+use smol::{self, Timer};
+use std::time::Duration;
+use tweeter::Tweeter;
+
+fn main() -> Result<()> {
+    let settings = Settings::new()?;
+    let tweeter = Tweeter::new(&settings);
+    let db = IrcDb::new(&settings.db.file)?;
+
+    smol::run(async { main_loop(&settings, &tweeter, &db).await })?;
+    Ok(())
+}
+
+async fn main_loop(settings: &Settings, tweeter: &Tweeter, db: &IrcDb) -> Result<()> {
+    loop {
+        tweeter.send_tweet(db.get_random(1)?.quote.clone()).await?;
+        Timer::after(Duration::from_secs(settings.interval_min * 60)).await;
+    }
 }
